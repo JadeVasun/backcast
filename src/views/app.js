c069313e1d66e45c9510ccc1d1dfc8fcdf284aa9
JadeVasun <jadevasun@gmail.com>
2017-08-29 20:37:08 -0700

    added videolist view

--- src/views/app.js
+++ src/views/app.js
@@ -3,20 +3,43 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.render();
     this.videos = new Videos(window.exampleVideoData);
-    this.videoplayerview = new VideoPlayerView({collection: this.videos});
-    this.videoplayerview.render();
-    this.videolistview = new VideoListView();
-    this.videolistview.render();
+
+    this.listenTo(this.videos, 'select', this.selectFirst);
+    //this.videos.search('javascript tutorial');
+    this.render();
   },
+  //collection just a bunch of models
 
 
+
+  selectFirst: function() {
+    if (this.videos.length > 0) {
+      this.videos.at(0);
+    }
+  },
+  
   render: function() {
     this.$el.html(this.template());
+
+    var search = new SearchView({
+      collection: this.videos,
+      el: this.$('.search')
+    }).render();
+
+    var videoList = new VideoListView({
+      collection: this.videos
+    }).render();
+    $('.list').children().detach();
+    $('.list').append(videoList);
+
+    new VideoPlayerView({
+      model: this.videos.at(0),
+      collection: this.videos,
+      el: this.$('.player')
+    }).render();
     return this;
   },
-
 
   template: templateURL('src/templates/app.html')
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,22 +1,23 @@
 var VideoListView = Backbone.View.extend({
 
-  initialize: function () {
-  this.collection.on('sync', this.render, this);
-  },
+  // initialize: function () {
+  // this.collection.on('sync', this.render, this);
+//},
 
   render: function() {
-    this.$el.children().detach();
-    this.$el.html(this.template());
-    this.collection.forEach(function (video) {
-      var videolistentry = new VideoListEntryView ({model:video});
-      videolistentry.render();
-      
-    })
     
-    return this;
+    this.$el.html(this.template());
+    this.$el.children().empty();
+    this.collection.forEach(this.renderVideo, this);
+
+    return this.$el;
   },
 
-  
+  renderVideo: function(video) {
+    var videoView = new VideoListEntryView({model: video});
+    this.$el.find('.video-list').append(videoView.render());
+  },
+
   template: templateURL('src/templates/videoList.html')
 
 });
